3bca6ee1a920eb091653761722cc702c7f8c5cef
yvonnexchen <yvonnexchen@tencent.com>
2020-03-26 22:17:37 +0800

    fix: 加号不准确

--- app/service/wechat.js
+++ app/service/wechat.js
@@ -120,7 +120,7 @@ class Wechat {
                         const tag = userMesHandleType.substring(2);
                         replyContent = await this.__randomRecommend(tag);
                     } else {
-                        replyContent = '回复：求片+片名。例如求片+阿凡达[Smart](注意加号别打错了，点击菜单查看更多操作)' 
+                        replyContent = '回复：求片+片名。例如求片+阿凡达[Smart](点击菜单查看更多操作)' 
                     }
                 }
             }
